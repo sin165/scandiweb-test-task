489224b392b2cab4fac2f0c03dc9c5cac180c683
Tamar Sinauridze <sin165165@gmail.com>
2023-02-22 18:26:35 +0400

    Change validations

--- src/utils/validateForm.js
+++ src/utils/validateForm.js
@@ -1,17 +1,17 @@
 export function validateForm(sku, name, price, type, size, height, width, length, weight) {
     if (!sku || !name || !price || type==="default") return "Please, submit required data";
-    if (isNaN(price)) return "Please, provide the data of indicated type";
+    if (isNaN(price) || price<0) return "Please, provide the data of indicated type";
     if (type === "dvd") {
         if (!size) return "Please, submit required data";
-        if (isNaN(size)) return "Please, provide the data of indicated type";
+        if (isNaN(size) || size<0) return "Please, provide the data of indicated type";
     }
     if (type === "book") {
         if (!weight) return "Please, submit required data";
-        if (isNaN(weight)) return "Please, provide the data of indicated type";
+        if (isNaN(weight) || weight<0) return "Please, provide the data of indicated type";
     }
     if (type === "furniture") {
         if (!height || !width || !length) return "Please, submit required data";
-        if (isNaN(height) || isNaN(width) || isNaN()) return "Please, provide the data of indicated type";
+        if (isNaN(height) || isNaN(width) || isNaN(length) || height<0 || width<0 || length<0) return "Please, provide the data of indicated type";
     }
     return null;
 }
